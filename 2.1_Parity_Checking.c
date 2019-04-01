0e922537aafc4c9845ae750806163c0e0f255947
Vivek Pattanaik <vivekpattanaik100@gmail.com>
2019-04-01 23:59:52 +0530

    Checksum added

--- 2.1_Parity_Checking.c
+++ 2.1_Parity_Checking.c
@@ -1,18 +1,42 @@
+//PARITY CHECK
 #include <stdio.h>
+#include <string.h>
+char c[20];
+void sender()
+{
+    printf("\nSender Function\n");
+    gets(c);
+    int i = 0;
+    int count = 0;
+    for (; i < strlen(c); i++)
+        if (c[i] == '1')
+            count++;
+    c[i] = '1';
+    if (count % 2 == 0)
+        c[i] = '0';
+    puts(c);
+}
+void reciever()
+{
+    printf("Reciever function\n");
+    char c1[20];
+    gets(c1);
+    int i = 0;
+    int count = 0;
+    for (; i < strlen(c1); i++)
+        if (c1[i] == '1')
+            count++;
+    c1[i] = '1';
+    if (count % 2 == 0)
+        c1[i] = '0';
+    if (c1[i] == c[i])
+        printf("\nNo Error Detected");
+    else
+        printf("\nError Detected");
+}
 int main()
 {
-    int parity = 0, num;
-    printf("enter the number");
-    scanf("%d", &num);
-    while (num) //LOOP WILL OPERATE UNTILL NUMBER BECOMES 0..BY RIGHT SHIFTING AGAIN & AGAIN
-    {
-        if (num & 1) //....MAKE (num & 1)..&..extract 1 bit from 8 bit number..if that 1bit is 1 t    hen ...parity changes...else if that perticular bit is 0 found...then parity did not c    hnges...
-            parity = !parity;
-        num = num >> 1; //after checking 1st bit...check the next bit to thet bit which is already     checked...in the same 8 bit number...by shifting 1 right ..//
-    }
-    if (parity == 1) //1 is representing that parity is odd..//
-        printf("parity is odd");
-    else
-        printf("parity is even"); //0 is representing that parity is even..//
+    sender();
+    reciever();
     return 0;
 }
